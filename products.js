6aaa9455db0f9af7631cd959c5fb706b65b129bb
Valetina-2608 <valusha2608@gmail.com>
2023-03-06 21:28:35 +0200

    Add list of products

---
+++ products.js
@@ -0,0 +1,37 @@
+let products = [
+    { 
+        "image":"img/espresso.jpg",
+        "caption": "Espresso",
+        "price": "2$"
+    },
+    { 
+    "image":"img/americano.jpg",
+    "caption": "Americano",
+    "price": "2$"
+    },
+    { 
+        "image":"img/milk_americano.jpg",
+        "caption": "Americano with milk",
+        "price": "3$"
+    },
+    { 
+        "image":"img/cappuccino.jpg",
+        "caption": "Cappuccino",
+        "price": "4$"
+    },
+    { 
+        "image":"img/latte.jpg",
+        "caption": "Latte",
+        "price": "4$"
+    },
+    { 
+        "image":"img/latte_macchiato.jpg",
+        "caption": "Latte Macchiato",
+        "price": "4$"
+    },
+    { 
+        "image":"img/caramel-macchiato.jpg",
+        "caption": "Caramel Macchiato",
+        "price": "5$"
+    }
+]
