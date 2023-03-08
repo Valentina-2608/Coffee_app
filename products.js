e43ba88362fcc499143cf84b3b9402cc126f0b6b
Valetina-2608 <valusha2608@gmail.com>
2023-03-08 19:29:00 +0200

    Add new elements

--- products.js
+++ products.js
@@ -24,14 +24,99 @@ let products = [
         "caption": "Latte",
         "price": "4$"
     },
-    { 
+    {
+        "image":"img/ice_latte.jpg",
+        "caption": "Iced Latte",
+        "price": "5$"
+    },
+    {
+        "image":"img/pumpkin_spice_latte.jpg",
+        "caption": "Pumpkin Spice Latte",
+        "price": "6$"
+    },
+    {
+        "image":"img/pistachio_latte.jpg",
+        "caption": "Pistachio Latte",
+        "price": "6$"
+    },
+    {
         "image":"img/latte_macchiato.jpg",
         "caption": "Latte Macchiato",
         "price": "4$"
     },
-    { 
-        "image":"img/caramel-macchiato.jpg",
+    {
+        "image":"img/caramel_brulee_latte.jpg",
+        "caption": "Caramel Brulee Latte",
+        "price": "5$"
+    },
+    {
+        "image":"img/caramel_macchiato.jpg",
         "caption": "Caramel Macchiato",
         "price": "5$"
+    },
+    { 
+        "image":"img/iced-caramel-macchiato.jpg",
+        "caption": "Iced Caramel Macchiato",
+        "price": "5$"
+    },
+    { 
+        "image":"img/flat_white.jpg",
+        "caption": "Flat White",
+        "price": "3$"
+    },
+    { 
+        "image":"img/iced_flat_white.jpg",
+        "caption": "Iced Flat White",
+        "price": "4$"
+    },
+    {
+        "image":"img/irish-coffee.png",
+        "caption": "Irish Coffee",
+        "price": "8$"
+    },
+    {
+        "image":"img/viennese_coffee.jpg",
+        "caption": "Viennese Coffee",
+        "price": "8$"
+    },
+    {
+        "image":"img/mocha.jpg",
+        "caption": "Mocha Coffee",
+        "price": "5$"
+    },
+    {
+        "image":"img/peppermint_mocha.jpg",
+        "caption": "Peppermint Mocha",
+        "price": "6$"
+    },
+    {
+        "image":"img/iced_coffee.jpg",
+        "caption": "Iced Coffee",
+        "price": "5$"
+    },
+    {
+        "image":"img/frappuccino.jpg",
+        "caption": "Coffee Frappuccino",
+        "price": "5$"
+    },
+    {
+        "image":"img/caramel-frappuccino.jpg",
+        "caption": "Caramel Frappuccino",
+        "price": "6$"
+    },
+    {
+        "image":"img/raf.jpg",
+        "caption": "Raf Coffee",
+        "price": "4$"
+    },
+    {
+        "image":"img/honey_raf.jpg",
+        "caption": "Honey Raf Coffee",
+        "price": "6$"
+    },
+    {
+        "image":"img/orange_raf.jpg",
+        "caption": "Orange Raf Coffee",
+        "price": "6$"
     }
 ]
